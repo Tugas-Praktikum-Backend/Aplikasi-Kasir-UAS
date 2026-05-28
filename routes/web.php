1b1c2d667364e97d6fe6560845f59d42dbf9bb7b
mosesmarvian <mosesmarvian@gmail.com>
2026-05-28 22:54:06 +0700

    fixing web.php bug

--- routes/web.php
+++ routes/web.php
@@ -12,7 +12,6 @@ use App\Http\Controllers\CustomerController;
 Route::get('/', function () {
     return redirect('/customers');
 });
-});
 
 Route::resource('managers', ManagerController::class);
 Route::resource('products', ProductController::class);
