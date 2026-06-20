85eb4eda4505d5bb630ef4132c7469284d02d31b
Angga0512 <mairlangga686@gmail.com>
2026-06-20 09:20:15 +0700

    tambah controller resource dan route kategori

--- routes/web.php
+++ routes/web.php
@@ -4,18 +4,20 @@ use App\Http\Controllers\EmployeesController;
 use App\Http\Controllers\ProductController;
 use App\Http\Controllers\CustomerController;
 use App\Http\Controllers\DiscountController;
+use App\Http\Controllers\CategoryController;
 use Illuminate\Support\Facades\Route;
 
 Route::resource('products', ProductController::class);
 Route::resource('customers', CustomerController::class);
 Route::resource('discounts', DiscountController::class);
+Route::resource('categories', CategoryController::class);
 
 Route::get('/', function () {
     return view('index');
 });
 
 Route::post('employees/login', [EmployeesController::class, 'login'])->name('employees.login');
-Route::post('employees/logout', [EmployeesController::class. 'logout'])->name('employees.logout');
+Route::post('employees/logout', [EmployeesController::class, 'logout'])->name('employees.logout');
 Route::get('employees/login', [EmployeesController::class, 'loginPage']);
 Route::get('employees/logout', [EmployeesController::class, 'logout']);
 Route::post('customers/login', [CustomersController::class, 'login'])->name('customers.login');
