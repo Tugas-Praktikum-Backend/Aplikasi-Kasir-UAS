da146bd371a2a9cc64fbf42b011d901b36dba9ea
FresherGAMING <reyhan01abcde@gmail.com>
2026-06-21 15:14:27 +0700

    Adding temporary data for testing purpose

--- database/migrations/2026_05_28_130136_create_employees_table.php
+++ database/migrations/2026_05_28_130136_create_employees_table.php
@@ -25,11 +25,20 @@ return new class extends Migration
         });
         DB::table('employees')->insert([
             [
-                'email' => 'test@gmail.com',
+                'email' => 'temp_manager@gmail.com',
                 'password' => Hash::make('12345678'),
-                'username' => 'test',
+                'username' => 'temp_manager',
+                'role' => RoleUtils::ROLE_MANAGER,
+                'monthly_revenue' => 15000000,
+                'created_at' => now(),
+                'updated_at' => now()
+            ],
+            [
+                'email' => 'temp_cashier@gmail.com',
+                'password' => Hash::make('12345678'),
+                'username' => 'temp_cashier',
                 'role' => RoleUtils::ROLE_CASHIER,
-                'monthly_revenue' => 0,
+                'monthly_revenue' => 7000000,
                 'created_at' => now(),
                 'updated_at' => now()
             ]
