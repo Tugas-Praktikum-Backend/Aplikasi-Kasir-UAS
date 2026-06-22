d66d9ac9624da9abb1cf748b27026ffae13be076
Clive <cliveang2008@gmail.com>
2026-06-22 15:29:36 +0700

    Implement transaction controller

--- app/Http/Controllers/TransactionController.php
+++ app/Http/Controllers/TransactionController.php
@@ -2,7 +2,9 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Product;
 use App\Models\Transaction;
+use App\Models\CustomerPaymentMethod;
 use Illuminate\Http\Request;
 
 class TransactionController extends Controller
@@ -12,7 +14,8 @@ class TransactionController extends Controller
      */
     public function index()
     {
-        //
+    $transactions = Transaction::with('product')->get();
+    return view('transactions.index', compact('transactions'));
     }
 
     /**
@@ -20,7 +23,13 @@ class TransactionController extends Controller
      */
     public function create()
     {
-        //
+    $products = Product::all();
+    $paymentMethods = CustomerPaymentMethod::all();
+
+    return view(
+    'transactions.create',
+    compact('products', 'paymentMethods')
+    );
     }
 
     /**
@@ -28,7 +37,26 @@ class TransactionController extends Controller
      */
     public function store(Request $request)
     {
-        //
+    $request->validate([
+    'product_id' => 'required|exists,id',
+    'payment_method_id' => 'required|exists,id',
+    'quantity' => 'required|integer|min:1',
+    ]);
+
+    $product = Product::findOrFail(
+    request('product_id')
+    );
+
+    Transaction::create([
+    'product_id' => request('product_id'),
+    'payment_method_id' => request('payment_method_id'),
+    'quantity' => request('quantity'),
+    'total_price' => $product->harga * request('quantity'),
+    ]);
+
+    return redirect()
+    ->route('transactions.index')
+    ->with('success', 'Transaksi berhasil dibuat.');
     }
 
     /**
