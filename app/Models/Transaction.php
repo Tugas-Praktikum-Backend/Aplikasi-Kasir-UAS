4cd527cadb85d2dc3f5ee0b357b67107d133ee51
Clive <cliveang2008@gmail.com>
2026-06-22 14:59:27 +0700

    Create transaction model

--- app/Models/Transaction.php
+++ app/Models/Transaction.php
@@ -6,5 +6,18 @@ use Illuminate\Database\Eloquent\Model;
 
 class Transaction extends Model
 {
-    //
+    protected $fillable = ['product_id','payment_method_id','quantity','total_price'];
+
+    public function product()
+    {
+        return $this->belongsTo(Product::class);
+    }
+
+    public function paymentMethod()
+    {
+        return $this->belongsTo(
+            CustomerPaymentMethod::class,
+            'payment_method_id'
+        );
+    }
 }
