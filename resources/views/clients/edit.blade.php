13147635b3c2e562aaecc0fb4c21220d03dca472
Angga0512 <mairlangga686@gmail.com>
2026-06-23 13:56:29 +0700

    mengubah struktur supplier: katalog klien + harga per produk,, validasi modal toko, halaman history restock

--- resources/views/clients/edit.blade.php
+++ resources/views/clients/edit.blade.php
@@ -1,8 +1,35 @@
-<h1>Ubah Klien</h1>
+<h1>Edit Klien</h1>
+
+@if($errors->any())
+    <ul>
+        @foreach($errors->all() as $error)
+            <li>{{ $error }}</li>
+        @endforeach
+    </ul>
+@endif
+
 <form action="{{ route('clients.update', $client->id) }}" method="POST">
     @csrf
     @method('PUT')
-    <p>Nama:<br><input type="text" name="nama" value="{{ old('nama', $client->nama) }}"></p>
+    <p>Nama Klien:<br><input type="text" name="nama" value="{{ old('nama', $client->nama) }}"></p>
+
+    <p>Produk yang bisa disupply:</p>
+    @php
+        $hargaMap = $client->products->pluck('pivot.harga', 'id');
+    @endphp
+
+    @forelse($products as $product)
+        <label style="display:block">
+            <input type="checkbox" name="products[]" value="{{ $product->id }}"
+                @if(in_array($product->id, old('products', $client->products->pluck('id')->toArray()))) checked @endif>
+            {{ $product->nama }}
+            — Harga: <input type="number" name="harga[{{ $product->id }}]" value="{{ old('harga.'.$product->id, $hargaMap[$product->id] ?? '') }}" min="0">
+        </label>
+    @empty
+        <p><em>Belum ada produk.</em></p>
+    @endforelse
+
+    <br>
     <button type="submit">Update</button>
     <a href="{{ route('clients.index') }}">Batal</a>
 </form>
--- resources/views/clients/index.blade.php
+++ resources/views/clients/index.blade.php
@@ -4,30 +4,44 @@
     <p>{{ session('success') }}</p>
 @endif
 
-<a href="{{ route('clients.create') }}">Tambah Klien</a>
-<br><br>
-<table border="1" cellpadding="10" cellspacing="0">
+
+<table border="1" cellpadding="8">
     <tr>
-        <th>ID</th>
-        <th>Nama</th>
+        <th>Nama Klien</th>
+        <th>Produk yang Tersedia</th>
         <th>Aksi</th>
     </tr>
     @forelse($clients as $client)
         <tr>
-            <td>{{ $client->id }}</td>
             <td>{{ $client->nama }}</td>
+            <td>
+                @forelse($client->products as $product)
+                    {{ $product->nama }} (Rp {{ number_format($product->pivot->harga) }})@if(!$loop->last), @endif
+                @empty
+                    <em>Belum ada</em>
+                @endforelse
+            </td>
             <td>
                 <form action="{{ route('clients.edit', $client->id) }}" method="GET" style="display:inline; margin-right: 5px;">
                     <button type="submit">Ubah</button>
                 </form>
-                <form action="{{ route('clients.destroy', $client->id) }}" method="POST" style="display:inline" onsubmit="return confirm('Yakin hapus?')">
+                <form action="{{ route('clients.destroy', $client->id) }}" method="POST" style="display:inline">
                     @csrf
                     @method('DELETE')
-                    <button type="submit">Hapus</button>
+                    <button type="submit" onclick="return confirm('Yakin hapus klien ini?')">Hapus</button>
                 </form>
             </td>
         </tr>
     @empty
-        <tr><td colspan="4">Belum ada klien.</td></tr>
+        <tr><td colspan="3">Belum ada klien.</td></tr>
     @endforelse
 </table>
+<br>
+
+<form action="{{ route('clients.create') }}" method="GET" style="display:inline; margin-right: 5px;">
+    <button type="submit">Tambah Klien Baru</button>
+</form>
+
+<form action="{{ route('suppliers.index') }}" method="GET" style="display:inline; margin-right:5px;">
+    <button type="submit">Supplier</button>
+</form>
