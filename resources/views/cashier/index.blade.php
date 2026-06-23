cc09d64a97b6ed2dc4e40c64403df0b47157809f
FresherGAMING <reyhan01abcde@gmail.com>
2026-06-23 11:40:25 +0700

    Deleting cashier from employees route and making it as an independent one

--- resources/views/cashier/index.blade.php
+++ resources/views/cashier/index.blade.php
@@ -2,7 +2,7 @@
 <h1> Pengelolaan Kasir </h1>
 
 <form method="get">
-    <button type="submit" formaction="{{ route('employees.cashier_create') }}">
+    <button type="submit" formaction="{{ route('cashier.create') }}">
         Buat Transaksi Baru
     </button>
 </form>
